ac2da9c2c430ae2201fba2597de5625a4ab68b84
kunhuang <kunhuang110@gmail.com>
2014-07-14 09:51:35 +0800

    【Server】修改API-查看商品，BUG
    1. 修改API查看商品，兼容seller和buyer，增加goods_num字段
    2. 修改URL的正则，兼容尾部有无斜杠
    3. 修改部分Bug

--- Server/json_api/urls.py
+++ Server/json_api/urls.py
@@ -8,34 +8,33 @@ urlpatterns = patterns('',
     url(r'^test/$', views.test, name='test'),
     url(r'^test2/$', views.test2, name='test2'),
     
-    url(r'^add_account/$', views.add_account, name='add_account'),
-    url(r'^get_account_id/$', views.get_account_id, name='get_account_id'),
-    url(r'^get_account_info/$', views.get_account_info, name='get_account_info'),
-    url(r'^edit_account_info/$', views.edit_account_info, name='edit_account_info'),
+    url(r'^add_account?/$', views.add_account, name='add_account'),
+    url(r'^get_account_id?/$', views.get_account_id, name='get_account_id'),
+    url(r'^get_account_info?/$', views.get_account_info, name='get_account_info'),
+    url(r'^edit_account_info?/$', views.edit_account_info, name='edit_account_info'),
 
-    url(r'^add_goods/$', views.add_goods, name='add_goods'),
-    url(r'^get_goods_info/$', views.get_goods_info, name='get_goods_info'),
-    url(r'^get_goods_array/$', views.add_goods, name='get_goods_array'),
-    url(r'^edit_goods_info/$', views.add_goods, name='edit_goods_info'),
+    url(r'^add_goods?/$', views.add_goods, name='add_goods'),
+    url(r'^get_goods_info?/$', views.get_goods_info, name='get_goods_info'),
+    url(r'^get_goods_array?/$', views.get_goods_array, name='get_goods_array'),
+    url(r'^edit_goods_info?/$', views.edit_goods_info, name='edit_goods_info'),
     
     
-    url(r'^transact_goods_info/$', views.transact_goods, name='transact_goods'),
-    url(r'^get_transaction_array/$', views.get_transaction_array, name='get_transaction_array'),
+    url(r'^transact_goods_info?/$', views.transact_goods, name='transact_goods'),
+    url(r'^get_transaction_array?/$', views.get_transaction_array, name='get_transaction_array'),
     
     
-    url(r'^add_comment/$', views.add_comment, name='add_comment'),
-    url(r'^get_comment_array/$', views.get_comment_array, name='get_comment_array'),
+    url(r'^add_comment?/$', views.add_comment, name='add_comment'),
+    url(r'^get_comment_array?/$', views.get_comment_array, name='get_comment_array'),
     
     
-    url(r'^add_wishlist/$', views.add_wishlist, name='add_wishlist'),
-    url(r'^get_wishlist/$', views.get_wishlist, name='get_wishlist'),
-    url(r'^delete_wishlist/$', views.delete_wishlist, name='delete_wishlist'),
+    url(r'^add_wishlist?/$', views.add_wishlist, name='add_wishlist'),
+    url(r'^get_wishlist?/$', views.get_wishlist, name='get_wishlist'),
+    url(r'^delete_wishlist?/$', views.delete_wishlist, name='delete_wishlist'),
     
-    '''
-    url(r'^send_message/$', views.send_message, name='send_message'),
-    url(r'^get_message_array/$', views.get_message_array, name='get_message_array'),
-    url(r'^get_message/$', views.get_message, name='get_message'),
-    '''
+    url(r'^send_message?/$', views.send_message, name='send_message'),
+    url(r'^get_message_array?/$', views.get_message_array, name='get_message_array'),
+    url(r'^get_message?/$', views.get_message, name='get_message'),
+    
     #url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
     #url(r'/test/$', views.results, name='results'),
     
